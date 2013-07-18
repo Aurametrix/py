0910131f73467ffb2846d6e14f43c637450d4716
Irene <irene@aurametrix.com>
2013-07-18 13:03:19 -0700

    google.appengine.dist not available in python2.7

---
+++ appengine_config.py
@@ -0,0 +1,2 @@
+# Make webapp.template use django 1.3
+webapp_django_version = '1.3'
--- django_bootstrap.py
+++ django_bootstrap.py
@@ -51,8 +51,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-from google.appengine.dist import use_library
-use_library('django', '1.2')
 
 
 # Import the part of Django that we use here.
